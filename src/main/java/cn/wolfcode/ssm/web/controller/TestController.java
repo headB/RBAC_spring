4097f3837735451636920b74dda3b46f3193dd43
headB_macbook_pro <775121173@qq.com>
2019-12-04 22:17:19 +0800

    优化导入

--- src/main/java/cn/wolfcode/ssm/web/controller/TestController.java
+++ src/main/java/cn/wolfcode/ssm/web/controller/TestController.java
@@ -2,17 +2,14 @@ package cn.wolfcode.ssm.web.controller;
 
 
 import cn.wolfcode.ssm.domain.User;
-import cn.wolfcode.ssm.query.PageResult;
 import cn.wolfcode.ssm.query.QueryObject;
 import cn.wolfcode.ssm.service.IShowUserService;
 import com.github.pagehelper.PageInfo;
-import org.graalvm.compiler.core.GraalCompiler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
