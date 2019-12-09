bb7a97c0c54a066a163c6ef363c53b33611de0c3
headB_macbook_pro <775121173@qq.com>
2019-12-09 23:44:45 +0800

    提交部分代码

--- src/main/java/cn/wolfcode/ssm/shiro/CRMRealm.java
+++ src/main/java/cn/wolfcode/ssm/shiro/CRMRealm.java
@@ -3,6 +3,7 @@ package cn.wolfcode.ssm.shiro;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
@@ -14,11 +15,21 @@ import org.springframework.stereotype.Component;
 public class CRMRealm extends AuthorizingRealm {
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+
+        //其实这里就是如果返回不为空的话，就是账号密码是对的！。
+
+
         return null;
     }
 
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+
+
+        //其实这里就是如果返回不为空的话，就是账号密码是对的！。
+        UsernamePasswordToken username = (UsernamePasswordToken)authenticationToken;
+        System.out.println(username.getUsername());
+
         return null;
     }
 }
---
+++ src/main/java/cn/wolfcode/ssm/shiro/ShiroFormAuthenicationFilter.java
@@ -0,0 +1,59 @@
+package cn.wolfcode.ssm.shiro;
+
+import cn.wolfcode.ssm.util.JsonResult;
+import com.alibaba.fastjson.JSON;
+import lombok.SneakyThrows;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+
+@Component("crmFormAuthenicationFilter")
+public class ShiroFormAuthenicationFilter extends FormAuthenticationFilter {
+
+
+    @Override
+    protected boolean onLoginSuccess(AuthenticationToken token, Subject subject, ServletRequest request, ServletResponse response) {
+
+
+        try {
+
+
+
+            response.setContentType("application/json;charset=utf-8");
+            String msg = JSON.toJSONString(new JsonResult());
+            response.getWriter().print(msg);
+
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+
+    @Override
+    protected boolean onLoginFailure(AuthenticationToken token, AuthenticationException e, ServletRequest request, ServletResponse response) {
+
+
+
+        try {
+            response.setContentType("application/json;charset=utf-8");
+            String msg = JSON.toJSONString(new JsonResult());
+            response.getWriter().print(msg);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+
+    }
+}
